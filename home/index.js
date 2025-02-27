acd53eb8f35d03a18fc7d53c8e4e310404359d55
Achirasun <141355191+Achirasun@users.noreply.github.com>
2025-02-28 04:33:09 +0700

    home
    commit my home

---
+++ home/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+
+// Creating the Express server
+const app = express();
+
+// static resourse & templating engine
+app.use(express.static('public'));
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/home', function (req, res) {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Starting node.js at port ${port}`);
+  });
