71e407cb987699880841885b00e3731fcb8fcb5d
Achirasun <141355191+Achirasun@users.noreply.github.com>
2025-03-01 04:51:18 +0700

    update pull data table

--- website/home/index.js
+++ website/home/index.js
@@ -6,6 +6,14 @@ const sqlite3 = require('sqlite3').verbose();
 // Creating the Express server
 const app = express();
 
+// Connect to SQLite database
+let db = new sqlite3.Database('itrentalhub_null.db', (err) => {    
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the SQlite database.');
+  });
+
 // static resourse & templating engine
 app.use(express.static('public'));
 // Set EJS as templating engine
@@ -14,9 +22,32 @@ app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// ตัว show แค่เช็คว่ามีค่ามาแล้วจริง ADMIN เท่านั้นที่ควรดูได้
 app.get('/home', function (req, res) {
-    res.render('home');
-});
+    const query = 'SELECT * FROM rental_data';
+    const query2 = `
+        SELECT r.Rental_ID, r.Rental_Name, rp.Rental_price, AVG(rv.Rating) AS avg_Rating
+        FROM rental_data r
+        JOIN rental_price rp ON r.Rental_ID = rp.Rental_ID
+        JOIN review rv ON r.Rental_ID = rv.Rental_ID
+        GROUP BY r.Rental_ID, r.Rental_Name, rp.Rental_price
+        ORDER BY avg_rating DESC
+        LIMIT 4;
+    `; // เลือก 4 อันดับคะแนนรีวิวสูงสุด
+    // all มีผลกลับมา run ไม่มีผลกลับมา
+    db.all(query, (err, rows) => {
+      if (err) {
+        console.log(err.message);
+      }
+      db.all(query2, (err, topRatedRentals) => {
+        if (err) {
+            console.log(err.message);
+        }
+    //   console.log(rows);
+            res.render('home', { data : rows, topRatedRentals: topRatedRentals});
+       });
+    });
+  });
 
 app.listen(port, () => {
     console.log(`Starting node.js at port ${port}`);
